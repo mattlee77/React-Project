15fe5da9ee11f3531ae4996eac4164ca237aa240
mattlee77 <mattwylee@gmail.com>
2026-03-05 14:30:58 +0800

    Create React framework for Memory Game project

---
+++ memory-game/src/data/cards.js
@@ -0,0 +1,17 @@
+// src/data/cards.js
+const cardData = [
+  { id: 1, name: 'Anne Boonchuy', image: 'path/to/anne.jpg' },
+  { id: 2, name: 'Sadie Croaker', image: 'path/to/sadie.jpg' },
+  { id: 3, name: 'Captain Grime', image: 'path/to/grime.jpg' },
+  { id: 4, name: 'Hop Pop Plantar', image: 'path/to/hoppop.jpg' },
+  { id: 5, name: 'Leopold Loggle', image: 'path/to/leopold.jpg' },
+  { id: 6, name: 'Maddie Flour', image: 'path/to/maddie.jpg' },
+  { id: 7, name: 'Polly Plantar', image: 'path/to/polly.jpg' },
+  { id: 8, name: 'Sasha', image: 'path/to/sasha.jpg' },
+  { id: 9, name: 'Sprig Plantar', image: 'path/to/sprig.jpg' },
+  { id: 10, name: 'Sylvia Sundew', image: 'path/to/sylvia.jpg' },
+  { id: 11, name: 'Mayor Toadstool', image: 'path/to/toadstool.jpg' },
+  { id: 12, name: 'Wally', image: 'path/to/wally.jpg' },
+];
+
+export default cardData;
